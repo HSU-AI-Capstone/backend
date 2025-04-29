3a5d5b9b36383a0744e37ffc6a90ec42bb5e37a1
ziglint1231 <lusrobee@gmail.com>
2025-04-29 23:20:45 +0900

    Update urls.py

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,7 +1,19 @@
 from backend.urls import path
 
-from . import views
+# from . import views
+
+# urlpatterns = [
+#     path("", views.index),
+# ]
+
+
+# api/urls.py
+#from django.urls import path
+from .views import CreateLectureTextView # 수정된 뷰 임포트
+
+#app_name = 'api' # 앱 네임스페이스 정의 (선택 사항)
 
 urlpatterns = [
-    path("", views.index),
+    # POST /api/v1/create-lecture/ 요청을 CreateLectureTextView 로 연결
+    path('create-lecture/', CreateLectureTextView.as_view(), name='create-lecture'),
 ]
