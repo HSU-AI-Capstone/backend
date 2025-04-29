43425b4e072e80d8d2bcee374e645805b7b4f76b
ziglint1231 <lusrobee@gmail.com>
2025-04-29 22:41:19 +0900

    Update urls.py

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,7 +1,17 @@
 from backend.urls import path
 
-from . import views
+# from . import views
+
+# urlpatterns = [
+#     path("", views.index),
+# ]
+# refiner/urls.py
+from django.urls import path
+from .views import PDFRefineView # 만든 뷰 임포트
+
+app_name = 'refiner' # 앱 네임스페이스 설정 (선택 사항)
 
 urlpatterns = [
-    path("", views.index),
+    # POST 요청을 처리할 API 엔드포인트 URL
+    path('refine-pdf/', PDFRefineView.as_view(), name='refine-pdf-api'),
 ]
