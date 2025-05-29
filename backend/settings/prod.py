62cddab063ecda01e9e9f7bd2bcade2966ab7cd9
sanghun <vitash1215@gmail.com>
2025-05-30 04:08:42 +0900

    fix : 배포 설정

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -2,20 +2,20 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DATABASE"),
-        'USER': os.getenv("MYSQL_USER"),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-        'HOST': 'mysqldb',
-        'PORT': int(os.getenv("DB_PORT", 3306)),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQL_DATABASE"),
+        "USER": os.getenv("MYSQL_USER"),
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+        "HOST": "mysqldb",
+        "PORT": int(os.getenv("DB_PORT", 3306)),
+        "OPTIONS": {
+            "charset": "utf8mb4",
         },
     }
 }
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
